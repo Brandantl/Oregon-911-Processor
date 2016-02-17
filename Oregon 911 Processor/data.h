6db496c2fd4aaad0954c0dceeda0c891fc488f24
Brandan Tyler Lasley <admin@brandanlasley.com>
2016-02-16 21:08:09 -0800

    More events

--- Oregon 911 Processor/data.h
+++ Oregon 911 Processor/data.h
@@ -4,3 +4,4 @@
 extern const struct agencyList knownAgencyList[];
 extern const std::string knownCallFeilds[];
 extern const callSummeryEventList knownCallSummeryEvents[];
+extern const callSummeryEventList knownFeildsEvents[];
